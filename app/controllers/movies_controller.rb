57b3b5931eaaf94078dde11265a50839b5642d0d
Kevin Nguyen <kevintnguyen@berkeley.edu>
2022-10-05 09:42:26 +0000

    part 3 complete?

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -23,26 +23,21 @@ class MoviesController < ApplicationController
         redirect_to movies_path(:ratings => @all_ratings_as_hash, :sort_by => '') and return
       end 
 
-      #if !params.has_key?(:ratings) || !params.has_key?(:sort_by)
-        #redirect_to movies_path(:ratings => session[:ratings], :sort_by => session[:sort_by]) and return
-      #end 
+      if !params.has_key?(:ratings) || !params.has_key?(:sort_by)
+        redirect_to movies_path(:ratings => session[:ratings], :sort_by => session[:sort_by]) and return
+      end 
 
-      if !params.has_key?(:ratings)
-        @ratings_to_show = []
-      else
-        @ratings_to_show = params[:ratings].keys
-        @ratings_to_show_as_hash = {}
-        @ratings_to_show.each do |i|
-          @ratings_to_show_as_hash[i] = 1
-        end 
-      end
+      @ratings_to_show = params[:ratings].keys
+      @ratings_to_show_as_hash = {}
+      @ratings_to_show.each do |i|
+        @ratings_to_show_as_hash[i] = 1
+      end 
 
       session[:ratings] = @ratings_to_show_as_hash
       session[:sort_by] = params[:sort_by]
 
       @movies = Movie.with_ratings(@ratings_to_show)
  
-
       if params.has_key?(:sort_by)
         @movies = @movies.order(params[:sort_by])
         if params[:sort_by]=='title'
