88d81b0ba416a5bca071b3158cecd49a53843d81
Kevin Nguyen <kevintnguyen@berkeley.edu>
2022-10-05 06:18:34 +0000

    reseting

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,15 +1,2 @@
 class Movie < ActiveRecord::Base
-
-  def self.all_ratings
-    ['G','PG','PG-13','R']
-  end 
-
-  def self.with_ratings(ratings_list)
-    # if ratings_list is an array such as ['G', 'PG', 'R'], retrieve all
-    #  movies with those ratings
-    # if ratings_list is nil, retrieve ALL movies
-    if ratings_list.nil?
-      Movie.all
-    end
-    
 end
