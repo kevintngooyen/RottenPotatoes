190c1bdc08b99292d983609e267c4cfb93b401be
Kevin Nguyen <kevintnguyen@berkeley.edu>
2022-10-05 10:03:35 +0000

    testing

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -13,36 +13,22 @@ class MoviesController < ApplicationController
         @all_ratings_as_hash[i] = 1
       end
 
-      #if !session.key?(:ratings) || !session.key?(:sort_by)
-        #if !session.key?(:ratings)
-          #session[:ratings] = @all_ratings_as_hash
-        #end
-        #if !session.key?(:sort_by)
-          #session[:sort_by] = ''
-        #end 
-        #redirect_to movies_path(:ratings => @all_ratings_as_hash, :sort_by => '') and return
-      #end 
-
-      #if !params.has_key?(:ratings) || !params.has_key?(:sort_by)
-        #redirect_to movies_path(:ratings => session[:ratings], :sort_by => session[:sort_by]) and return
-      #end 
-
       if !params.has_key?(:ratings)
-        @ratings_to_show = @all_ratings
+        @movies = Movie.with_ratings(@all_ratings)
+        session[:ratings] = @all_ratings_as_hash
       else
         @ratings_to_show = params[:ratings].keys
         @ratings_to_show_as_hash = {}
         @ratings_to_show.each do |i|
           @ratings_to_show_as_hash[i] = 1
         end 
+        @movies = Movie.with_ratings(@ratings_to_show)
+        session[:ratings] = @ratings_to_show_as_hash
       end 
 
-      session[:ratings] = @ratings_to_show_as_hash
-      session[:sort_by] = params[:sort_by]
-
-      @movies = Movie.with_ratings(@ratings_to_show)
  
       if params.has_key?(:sort_by)
+        session[:sort_by] = params[:sort_by]
         @movies = @movies.order(params[:sort_by])
         if params[:sort_by]=='title'
           @title_header = 'hilite bg-warning' 
