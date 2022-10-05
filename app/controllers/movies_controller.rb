a495c4f0af7772612023b84737b386d30e2accb2
Kevin Nguyen <kevintnguyen@berkeley.edu>
2022-10-05 10:07:09 +0000

    idk

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -14,18 +14,19 @@ class MoviesController < ApplicationController
       end
 
       if !params.has_key?(:ratings)
-        @ratings_to_show = @all_ratings
+        @movies = Movie.with_ratings(@all_ratings)
       else
         @ratings_to_show = params[:ratings].keys
         @ratings_to_show_as_hash = {}
         @ratings_to_show.each do |i|
           @ratings_to_show_as_hash[i] = 1
         end 
+
+        @movies = Movie.with_ratings(@ratings_to_show)
+
       end 
 
       session[:ratings] = @ratings_to_show_as_hash
-
-      @movies = Movie.with_ratings(@ratings_to_show)
  
       if params.has_key?(:sort_by)
         session[:sort_by] = params[:sort_by]
